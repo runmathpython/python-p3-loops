b2b428caa7ea4c41b5968b1dc07f6a88e0c11d0b
Seong Kim <runmath@gmail.com>
2023-07-14 20:22:24 -0600

    test done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    integers_squared = list()
+    integers_squared = [integer * integer for integer in int_list]
+    return integers_squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        i += 1
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
